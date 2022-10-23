10b89704430be8982e5666faca62f859d2e7c93f
Fiziev Vagif <vagasuper560469@gmail.com>
2022-10-23 21:53:49 +0600

    Закончил первый уровень.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+interface cardClassType {
+  rank: string,
+  suit: string,
+}
 
 function App() {
+  const ClassCard: React.FC<cardClassType> = props => {
+    const cardClass = 'card rank-' + props.rank + ' ' + props.suit;
+    let suit = '';
+
+    if (props.suit === 'diams') {
+      suit = '♦';
+    } else if (props.suit === 'hearts') {
+      suit = '♥';
+    } else if (props.suit === 'clubs') {
+      suit = '♣';
+    } else if (props.suit === 'spades') {
+      suit = '♠';
+    }
+
+    return (
+      <div className="App">
+        <div className="playingCards faceImages">
+        <span className={cardClass}>
+          <span className="rank">{props.rank.toUpperCase()}</span>
+          <span className="suit">{suit}</span>
+        </span>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ClassCard rank='k' suit='hearts'/>
     </div>
-  );
+  )
 }
 
 export default App;
